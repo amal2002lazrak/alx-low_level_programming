e00aafe4e81cba51e490c4339c507944365ae55e
amal2002lazrak <amallazrak221@gmail.com>
2023-04-20 19:27:53 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A test c file using printf function 
- * Return - 0 signifying program success
- */ 
+  * main - A  program that printf a line using the printf function 
+  * Return: 0 (Success)
+  */ 
+
 int main(void) 
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
