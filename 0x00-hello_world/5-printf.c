528bebaebb6e1be6a3ffef49ba417ca7f7eac754
amal2002lazrak <amallazrak221@gmail.com>
2023-04-20 18:27:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A test c file using printf function 
+ * Return - 0 signifying program success
+ */ 
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
