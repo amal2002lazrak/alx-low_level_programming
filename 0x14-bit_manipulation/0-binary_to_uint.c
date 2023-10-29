62308f44f8db114bde0f3032c1005648228b633c
root <root@DESKTOP-568I8JA.localdomain>
2023-10-29 22:12:01 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * * _power - pow of x and y
+ * * @x: first num
+ * * @y: second num
+ * * Return: integer
+ * */
+
 int _power(int x, int y)
 {
 		if (y < 0)
@@ -8,6 +15,14 @@ int _power(int x, int y)
 						return (1);
 				return (x * _power(x, y - 1));
 }
+
+/**
+ * * binary_to_uint - converts a binary number to an unsigned int
+ * * @b: pointing to a string of 0 and 1 chars
+ * *
+ * * Return: the converted number
+ * */
+
 unsigned int binary_to_uint(const char *b)
 {
 		int i, j, len, n;
@@ -15,16 +30,19 @@ unsigned int binary_to_uint(const char *b)
 
 				if (b == NULL)
 							return (0);
-					@@ -13,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
-							for (i = len - 1, j = 0; i >= 0; i--, j++)
-									{
-												if (b[i] == '1')
-																n = _power(2, j);
-														else if (b[i] == '0')
-																		n = 0;
-																else
-																				return (0);
-																		num += n;
-																			}
-						return (num);
+
+					len = strlen(b);
+
+						for (i = len - 1, j = 0; i >= 0; i--, j++)
+								{
+											if (b[i] == '1')
+															n = _power(2, j);
+													else if (b[i] == '0')
+																	n = 0;
+															else
+																			return (0);
+
+																	num += n;
+																		}
+							return (num);
 }
