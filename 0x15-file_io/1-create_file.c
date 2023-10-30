d8ab213e859a99d94cb37790f1d720e1db2e489f
amal2002lazrak <amallazrak221@gmail.com>
2023-10-30 23:51:44 +0100

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,26 +12,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-		int fp;
-			int len;
+	int fp;
+	int len;
 
-				if (filename == NULL)
-							return (-1);
+	if (filename == NULL)
+		return (-1);
 
 
-					fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-						if (fp == -1)
-									return (-1);
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fp == -1)
+		return (-1);
 
-							if (text_content != NULL)
-									{
-												len = write(fp, text_content, strlen(text_content));
-														if (len == -1)
-																	{
-																					close(fp);
-																								return (-1);
-																										}
-															}
-								close(fp);
-									return (1);
+	if (text_content != NULL)
+	{
+		len = write(fp, text_content, strlen(text_content));
+		if (len == -1)
+		{
+			close(fp);
+			return (-1);
+		}
+	}
+	close(fp);
+	return (1);
 }
