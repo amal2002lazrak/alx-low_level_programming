7d7f1a940094428df1b30cfa883f048ae6b27597
amal2002lazrak <amallazrak221@gmail.com>
2023-12-07 23:28:52 +0100

    updated

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * * print_dlistint - prints all the elements of a dlistint_t list
+ * * @h: pointer to head of list
+ * *
+ * * Return: number of nodes in list
+ * */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *tmp;
+	size_t count = 0;
+
+	tmp = h;
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - prints all the elements of a dlistint_t list
+ * @h: pointer to head of list
+ *
+ * Return: number of nodes in list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *tmp;
+	size_t count = 0;
+
+	tmp = h;
+	while (tmp)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
